67d29e6dcf2b51ed0c39a426ea27a99728849f57
YiTingLee <ranye0719@gmail.com>
2017-12-11 21:42:09 +0800

    test

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 type Serve int
 
 func (m Serve) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	raw, err := ioutil.ReadFile("data.json")
+	data, err := ioutil.ReadFile("data.json")
 	if err != nil {
 
 	}
-	fmt.Fprintln(w, string(raw))
+	fmt.Fprintln(w, string(data))
 }
 
 func main() {
